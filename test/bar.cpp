d031c376d7e7f3b2890840e78d6888822643e982
vjd215 <diluoffo@cs.umass.edu>
2012-12-08 17:25:57 -0500

    adding testcases files foo bar

---
+++ test/bar.cpp
@@ -0,0 +1,21 @@
+/*
+ * bar.cpp
+ *
+ *  Created on: Dec 6, 2012
+ *      Author: vjd215
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+
+void add2Number(){
+
+	//simple programming
+	int i = 0;
+
+	i++;
+	i++;
+	printf("Adding a number %d\n", i);
+
+}
---
+++ test/foo.cpp
@@ -0,0 +1,20 @@
+/*
+ * foo.cpp
+ *
+ *  Created on: Dec 6, 2012
+ *      Author: vjd215
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+
+void addNumber(){
+
+	//simple programming
+	int i = 0;
+
+	i++;
+	printf("Adding a number %d\n", i);
+
+}
